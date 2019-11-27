6717ad2fc11ead08b56faec30bc6140cfae7d589
ori.kosary1@gmail.com <ori.kosary1@gmail.com>
2019-11-27 09:28:55 +0200

    create version 1

---
+++ Graphics.py
@@ -0,0 +1,38 @@
+from tkinter import *
+import tkinter as tk
+
+import keyboard as keyboard
+
+
+class GUI(object):
+    root = None  # type -> tk
+
+    text = None  # type -> text
+    input_text = None  # type -> string
+
+    def __init__(self, root):
+        self.root = root
+        self.init_texts()
+        self.place_all()
+
+    def init_texts(self):
+        self.text = tk.Text(master=self.root, width=60, height=1)
+        self.text.bind('<Return>', lambda e: self.inputs(True))
+
+    def place_all(self):
+        self.text.place(x=0, y=100)
+
+    def inputs(self, is_backspace):
+        if is_backspace:
+            keyboard.press_and_release('backspace')
+        self.input_text = self.text.get('1.0', 'end-1c')
+        fin__input = self.input_text
+        # if u want to connect one after the other delete the next line
+        self.root.destroy()
+        return fin__input
+
+
+if __name__ == '__main__':
+    root = Tk()
+    gui = GUI(root=root)
+    root.mainloop()
